d89bb3b11301ab9c9a81db8a57faecfdbfe56514
enderbrine13 <faroukeyad11@gmail.com>
2022-09-11 16:40:48 +0200

    removing not important js

--- Secret/new.js
+++
@@ -1,9 +0,0 @@
-const images = ["https://images.unsplash.com/photo-1512453979798-5ea266f8880c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1000&q=80",
-"https://images.unsplash.com/photo-1493612276216-ee3925520721?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80", 
-"https://images.unsplash.com/photo-1519834785169-98be25ec3f84?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80"
-];
-
-document.addEventListener('DOMContentLoaded', _ => {
-  const randImageIndex = ~~(Math.random() * images.length);
-  document.getElementById('randImg').src = images[randImageIndex];
-});
